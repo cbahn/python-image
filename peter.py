d44f4e39863186a1d9227ea7f08a2b874302b3d5
Junco TheBird <me@here.com>
2015-10-23 18:33:19 -0500

    peter.py has some weird errors, but it really neat

--- peter.py
+++ peter.py
@@ -5,61 +5,42 @@ import operator
 
 
 
-#basic def of screen size
+# basic def of screen size
 screen_size = (1280,800)
 
 def random_color():
-	return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+	return ( random.randint(0,255) , random.randint(0,255) , random.randint(0,255) )
 
-def is_in_range(p):
-	return (0 <= p[0] and p[0] < screen_size[0] and 0 <= p[1] and p[1] < screen_size[1])
-			
+def distance_squared( p1 , p2 ):
+	return (p1[0]-p2[0])^2 + (p1[1]-p2[1])^2
 
-def color_all(color, region):
-	for p in region:
-		pixels[ p[0], p[1] ] = color;
+def random_point():
+	return ( random.randint(0, screen_size[0]-1), random.randint(0, screen_size[1]-1))
 
-color_list = [(212,133,41),(179,86,0),(88,66,51),(147,119,103),(240,235,193)]
+# point_list = [ ( random_point(), 0), (random_point(), 1), (random_point(), 2) ]
 
-def disk_region(x,y,r):
-	region = [];
-	for px in range(max(0,x-r-1),min(screen_size[0],x+r+1)):
-		for py in range(max(0,y-r-1),min(screen_size[1],y+r+1)):
-			if( (px-x)*(px-x) + (py-y)*(py-y) < r*r*random.random()*random.random() ):
-				region.append( (px,py) )
-	return region
+point_list = [ ( (0,0), 0), (screen_size,1) ]
 
-def decimate(l,drop_chance):
-	l_final = []
-	for al in l:
-		if( random.random() < drop_chance ):
-			l_final.append(al)
-	return l_final
-
-def length( vec ):
-	return math.sqrt( vec[0]*vec[0] + vec[1]*vec[1] )
-
-def star_region():
-	position = (random.randint(0,screen_size[0]),random.randint(0,screen_size[1]))
-	power = random.randint(200,400);
-	region = []
-	while( power > 0):
-		size = random.randint(int(.0*power),int(0.07 * power)) + random.randint(12,34)
-		region = region +  disk_region(position[0],position[1],int(size*.7389))
-		power -= size
-		to_center = map(lambda x,y: x/2 - y, screen_size, position)
-		length_to_center = length(to_center)
-		to_center = map(lambda x: int(10*x/length_to_center), to_center)
-		position = map(lambda x,y: x - y + random.randint(-45,45), position, to_center)
-	return region
-
+def closest_point( t, l ):
+	best_distance = 200000;
+	valu = -1;
+	for point_val in l:
+		new_distance = distance_squared( point_val[0] , t )
+		if best_distance > new_distance:
+			best_distance = new_distance
+			valu = point_val[1]
+	return valu
+		
 
 # main
-img = Image.new( 'RGB', screen_size, (240,235,193)) # create a new black image
+img = Image.new( 'RGB', screen_size) # create a new black image
 pixels = img.load() # create the pixel map
 
-for _ in range(1500):
-	color_all( random.choice(color_list), star_region() )
-			
-img.save('myim.bmp')
+print closest_point( (10,10), point_list )
+
+for x in range( screen_size[0] ):
+	for y in range( screen_size[1] ):
+		if (closest_point( (x,y), point_list ) == 0):
+			pixels[x,y] = random_color()
+img.save('peter.bmp')
 
