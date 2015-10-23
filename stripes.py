e5ebdc3f11d6bad0473e72f8951baee63ac6e9c1
Junco TheBird <me@here.com>
2015-10-22 19:28:47 -0500

    yep

--- stripes.py
+++ stripes.py
@@ -12,7 +12,9 @@ def gradient( i ):
 	return 1 + (screen_size[1]*(screen_size[0]/2 - abs(screen_size[0]/2 - i)) / screen_size[0])
 
 # main
-img = Image.new( 'RGB', screen_size, (238,232,213)) # create a new black image
+# img = Image.new( 'RGB', screen_size, (238,232,213)) # create a new black image
+img = Image.open("truck.jpg")
+img.rotate(45).show()
 pixels = img.load() # create the pixel map
 
 for x in range(screen_size[0]):
